e20224971e9ceef926b23d136208b1a9f4a7b020
Alvaro I. Mangual <amangual@live.com>
2016-10-02 20:02:45 -0500

    Assignment 25 Error Search

--- scripts/album.js
+++ scripts/album.js
@@ -30,6 +30,22 @@ var albumMarconi = {
      ]
 };
 
+// My Album
+var albumMonday = {
+     title: 'The tech we knew',
+     artist: 'Never Monday',
+     label: 'Lithium',
+     year: '2016',
+     albumArtUrl: 'assets/images/album_covers/09.png',
+     songs: [
+         { title: 'Cordless?', duration: '4:21' },
+         { title: 'What\'s a dial tone?', duration: '3:12' },
+         { title: 'Requires exact change', duration: '5:11'},
+         { title: 'Where\'s the floppy disc', duration: '4:42' },
+         { title: '64k', duration: '5:12'}
+     ]
+};
+
 var createSongRow = function(songNumber, songName, songLength) {
      var template =
         '<tr class="album-view-song-item">'
@@ -42,13 +58,15 @@ var createSongRow = function(songNumber, songName, songLength) {
      return template;
 };
 
-var setCurrentAlbum = function(album) {
-    // #1
     var albumTitle = document.getElementsByClassName('album-view-title')[0];
     var albumArtist = document.getElementsByClassName('album-view-artist')[0];
     var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
     var albumImage = document.getElementsByClassName('album-cover-art')[0];
     var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
+var setCurrentAlbum = function(album) {
+
+
     
     // #2
     albumTitle.firstChild.nodeValue = album.title;
@@ -67,4 +85,14 @@ var setCurrentAlbum = function(album) {
 
 window.onload = function () {
     setCurrentAlbum(albumPicasso)
+    
+    var albums = [albumPicasso, albumMarconi, albumMonday];
+    var index = 1;
+    albumImage.addEventListener("click", function(event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+        if (index == album.length) {
+            index = 0;
+        }
+    });
 };
